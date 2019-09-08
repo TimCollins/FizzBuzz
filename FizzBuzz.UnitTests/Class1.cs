75a1d016e1a9eea0eec7f05fa6e2de3b8d262436
Tim <tim@timcollins.ie>
2019-09-08 18:39:05 +0100

    Added unit test libraries

--- FizzBuzz.UnitTests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace FizzBuzz.UnitTests
-{
-    public class Class1
-    {
-    }
-}
---
+++ FizzBuzz.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void Something()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
