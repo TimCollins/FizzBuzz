457590810732f22d17a8b7c037ae14fb61a7b2b2
Tim <tim@timcollins.ie>
2019-09-08 19:07:02 +0100

    Added initial implementation of solution.

---
+++ FizzBuzz.App/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿namespace FizzBuzz.App
+{
+    public class FizzBuzz
+    {
+        public string[] Calculate(int max)
+        {
+            var output = new string[max];
+
+            for (var i = 0; i < max; i++)
+            {
+                output[i] = GetFizzBuzz(i + 1);
+            }
+
+            return output;
+        }
+
+        private string GetFizzBuzz(int i)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+
+            if (i % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if (i % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return i.ToString();
+        }
+    }
+}
