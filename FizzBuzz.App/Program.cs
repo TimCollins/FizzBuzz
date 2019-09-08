270e4d0d81704751efcc52180475a95ec5604ece
Tim <tim@timcollins.ie>
2019-09-08 19:07:30 +0100

    Added output display function.

--- FizzBuzz.App/Program.cs
+++ FizzBuzz.App/Program.cs
@@ -9,8 +9,20 @@ namespace FizzBuzz.App
             // Write a program to output the first 100 numbers with Fizz, Buzz and FizzBuzz substitutions as appropriate
             // See https://en.wikipedia.org/wiki/Fizz_buzz
 
-            Console.WriteLine("Hello World!");
+            var fizzBuzz = new FizzBuzz();
+
+            var output = fizzBuzz.Calculate(15);
+            Display(output);
+
             Utils.WaitForEscape();
+        }
+
+        private static void Display(string[] output)
+        {
+            for (var i = 0; i < output.Length; i++)
+            {
+                Console.WriteLine("{0} = {1}", i + 1, output[i]);
+            }
         }
     }
 }
